7f6d7b5a82fd40eff74b0a525c1e2c986c450537
galatserega <119287683+galatserega@users.noreply.github.com>
2024-11-25 08:52:07 +0200

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-30o^ezprd9&ldztohx$=)us62r+hrakm7jttllt-*=g0l$yeq^"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://galatserega.pythonanywhere.com/', 'galatserega.pythonanywhere.com']
 
