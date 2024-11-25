4240d22fc1e72cddec0d918c1b7bd09648f1b9f7
galatserega <119287683+galatserega@users.noreply.github.com>
2024-11-25 08:50:53 +0200

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-30o^ezprd9&ldztohx$=)us62r+hrakm7jttllt-*=g0l$yeq^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://galatserega.pythonanywhere.com/', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['https://galatserega.pythonanywhere.com/']
 
 
 
